f8749b056e651516236b6298f20c7621393a08a8
xiexinfang <1036554353@qq.com>
2019-08-29 15:37:58 +0800

    response code

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package tws.controller;
 
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import tws.mapper.EmployeeMapper;
 import tws.model.Employee;
@@ -15,6 +16,7 @@ public class EmployeeController {
     private EmployeeMapper employeeMapper;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void addEmployees(@RequestBody Employee employee){
         employeeMapper.insertEmployee(employee);
     }
@@ -25,11 +27,13 @@ public class EmployeeController {
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateEmployee(@RequestBody Employee employee){
        employeeMapper.updateEmployee(employee);
     }
 
     @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable("id") int id){
         employeeMapper.deleteEmployee(id);
     }
